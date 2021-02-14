b5cadfe4d86fbcaabf876ebe8a024dd5ad864250
Evil Puncker <EPuncker@users.noreply.github.com>
2021-02-14 16:15:57 -0300

    Several quest fixes

--- data/scripts/actions/tools/firebug.lua
+++ data/scripts/actions/tools/firebug.lua
@@ -9,6 +9,12 @@ function fireBug.onUse(player, item, fromPosition, target, toPosition, isHotkey)
 		return true
 	end
 
+	-- The Hidden City of Beregar
+	if target.itemid == 8642 and target.actionid == 50119 then
+		target:transform(8641)
+		return true
+	end
+
 	local chance = math.random(10)
 	if chance > 4 then -- Success 6% chance
 		if target.itemid == 7538 then -- Destroy spider webs/North - South

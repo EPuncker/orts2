d77e2f484a9ba6fd089784cef45220302f08ea5a
Marco Oliveira <marcomoa@hotmail.com>
2021-02-23 12:18:08 -0300

    Fix: Loading order (#14)
    Core libs must be loaded before any others, thus avoiding console errors.

--- data/lib/lib.lua
+++ data/lib/lib.lua
@@ -1,9 +1,9 @@
+-- Core API functions implemented in Lua
+dofile('data/lib/core/core.lua')
+
 -- Customs library:
 dofile('data/lib/features/features.lua')
 dofile('data/lib/quests/quests.lua')
-
--- Core API functions implemented in Lua
-dofile('data/lib/core/core.lua')
 
 -- Compatibility library for our old Lua API
 dofile('data/lib/compat/compat.lua')

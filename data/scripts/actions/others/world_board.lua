107cae08b5fff726baf31b419ccf7c1f56b47831
Marco Oliveira <marcomoa@hotmail.com>
2021-03-08 16:50:13 -0300

    Update world_board.lua

--- data/scripts/actions/others/world_board.lua
+++ data/scripts/actions/others/world_board.lua
@@ -1,19 +1,19 @@
 local worldBoard = Action()
 
 local communicates = {
-    [1] = {
-        globalStorage = GlobalStorage.WorldBoard.Yasir,
-        communicate = "Oriental ships sighted! A trader for exotic creature products may currently be visiting Carlin, Ankrahmun or Liberty Bay."
-    },
+	[1] = {
+		globalStorage = GlobalStorage.WorldBoard.Yasir,
+		communicate = "Oriental ships sighted! A trader for exotic creature products may currently be visiting Carlin, Ankrahmun or Liberty Bay."
+	},
 }
 
 function worldBoard.onUse(player, item, fromPosition, target, toPosition, isHotkey)
-    for index, value in pairs(communicates) do
-        if Game.getStorageValue(value.globalStorage) > 0 then
-            player:sendTextMessage(MESSAGE_EVENT_ADVANCE, value.communicate)
-        end
-    end
-    return true
+	for index, value in pairs(communicates) do
+		if Game.getStorageValue(value.globalStorage) > 0 then
+			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, value.communicate)
+		end
+	end
+	return true
 end
 
 worldBoard:id(21570)

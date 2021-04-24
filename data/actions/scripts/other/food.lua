30b660fc46ecf79564cf329bca2b882c0b284494
Evil Puncker <EPuncker@users.noreply.github.com>
2021-04-23 22:30:42 -0300

    fixed #30
    - added missing noblewatch action entry
    - renamed die file name
    - corrected egg id in foods
    - removed hole in Position: 32359, 32282, 7

--- data/actions/scripts/other/food.lua
+++ data/actions/scripts/other/food.lua
@@ -1,5 +1,4 @@
 local foods = {
-	[2328] = {6, 'Gulp.'}, -- egg
 	[2362] = {5, 'Crunch.'}, -- carrot
 	[2666] = {15, 'Munch.'}, -- meat
 	[2667] = {12, 'Munch.'}, -- fish
@@ -27,6 +26,7 @@ local foods = {
 	[2689] = {10, 'Crunch.'}, -- bread
 	[2690] = {3, 'Crunch.'}, -- roll
 	[2691] = {8, 'Crunch.'}, -- brown bread
+	[2695] = {6, 'Gulp.'}, -- egg
 	[2696] = {9, 'Smack.'}, -- cheese
 	[2787] = {9, 'Munch.'}, -- white mushroom
 	[2788] = {4, 'Munch.'}, -- red mushroom

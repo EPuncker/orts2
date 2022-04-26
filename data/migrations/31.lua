daeae8b65dfdd9f576549d6f2510c1edf9f61bbd
Evil Puncker <EPuncker@users.noreply.github.com>
2022-04-26 18:30:14 -0300

    Reseted migrations

--- data/migrations/31.lua
+++
@@ -1,6 +0,0 @@
-function onUpdateDatabase()
-	print("> Updating database to version 31 (64 bit market prices)")
-	db.query("ALTER TABLE `market_offers` MODIFY `price` BIGINT UNSIGNED;")
-	db.query("ALTER TABLE `market_history` MODIFY `price` BIGINT UNSIGNED;")
-	return true
-end

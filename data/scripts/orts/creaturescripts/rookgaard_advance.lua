a3880f40569819d7af7238c79e4f20505c2446ed
Marco Oliveira <marcomoa@hotmail.com>
2022-12-14 17:11:44 -0300

    missing it

--- data/scripts/orts/creaturescripts/rookgaard_advance.lua
+++ data/scripts/orts/creaturescripts/rookgaard_advance.lua
@@ -1,8 +1,12 @@
-function onAdvance(player, skill, oldLevel, newLevel)
-	if skill ~= SKILL_LEVEL or newLevel ~= 8 or player:getVocation():getId() ~= VOCATION_NONE then
+local rookgaardAdvance = CreatureEvent("AdvanceRookgaard")
+
+function rookgaardAdvance.onAdvance(player, skill, oldLevel, newLevel)
+	if skill ~= SKILL_LEVEL or newLevel ~= 8 or player:getVocation():getId() ~= 0 then
 		return true
 	end
 
 	player:sendTextMessage(MESSAGE_EVENT_ADVANCE, 'Congratulations! You are ready to leave this island and choose a vocation now. Go see the Oracle over the academy in Rookgaard before you advance to level 10!')
 	return true
 end
+
+rookgaardAdvance:register()

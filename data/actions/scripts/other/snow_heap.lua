7788977e775990b1fb4867b21b7a251bf3cabf9a
Marco Oliveira <marcomoa@hotmail.com>
2022-12-21 19:25:11 -0300

    Missing tools

--- data/actions/scripts/other/snow_heap.lua
+++ data/actions/scripts/other/snow_heap.lua
@@ -1,4 +1,4 @@
 function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 	Game.createItem(2111, 1, item:getPosition())
-	return true
+	return onUseSpoon(player, item, fromPosition, target, toPosition, isHotkey)
 end

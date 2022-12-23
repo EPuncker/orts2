891df67f9352112723bd2b453efb792d29c54c7f
Marco Oliveira <marcomoa@hotmail.com>
2022-12-22 22:33:57 -0300

    Movements: final

--- data/scripts/quests/dreamer_challenge.lua
+++ data/scripts/quests/dreamer_challenge.lua
@@ -341,3 +341,469 @@ end
 
 walls:uid(2246, 2247, 2248, 2249)
 walls:register()
+
+local carrot = MoveEvent()
+
+function carrot.onStepIn(creature, item, position, fromPosition)
+	local player = creature:getPlayer()
+	if not player then
+		return true
+	end
+
+	if item.uid == 2241 then
+		if player:getItemCount(2684) > 0 then
+			player:teleportTo(Position(32861, 32235, 9))
+			player:removeItem(2684, 1)
+		else
+			player:teleportTo(fromPosition)
+			doAreaCombatHealth(player, COMBAT_FIREDAMAGE, fromPosition, 0, -10, -20, CONST_ME_HITBYFIRE)
+		end
+	elseif item.uid == 2242 then
+		player:teleportTo(Position(32861, 32240, 9))
+	end
+
+	player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+	return true
+end
+
+carrot:type("stepin")
+carrot:uid(2241, 2242)
+carrot:register()
+
+local fireBugTeleport = MoveEvent()
+
+function fireBugTeleport.onStepIn(creature, item, position, fromPosition)
+
+	return true
+end
+
+fireBugTeleport:type("stepin")
+fireBugTeleport:uid(2243)
+fireBugTeleport:register()
+
+local riddle = MoveEvent()
+
+function riddle.onStepIn(creature, item, position, fromPosition)
+	item:transform(425)
+
+	local index = item.actionid == 2245 and 'x' or 'y'
+	local new_position = Position(position.x, position.y, position.z)
+	for i = 1, 6 do
+		new_position[index] = position[index] + 2 + i - 1
+		local tile = Tile(new_position)
+		local itemCount = tile:getDownItemCount()
+		if itemCount > 0 then
+			new_position[index] = position[index] + 2 + i % 6
+			tile:getThing(tile:getTopItemCount() + tile:getCreatureCount() + itemCount):moveTo(new_position)
+		end
+	end
+	return true
+end
+
+riddle:type("stepin")
+riddle:aid(2245, 2246)
+riddle:register()
+
+local riddle = MoveEvent()
+
+function riddle.onStepOut(creature, item, position, fromPosition)
+	item:transform(426)
+	return true
+end
+
+riddle:type("stepout")
+riddle:aid(2245, 2246)
+riddle:register()
+
+local stoneTeleport = MoveEvent()
+
+local config = {
+	{position = Position(32873, 32263, 14), itemId = 1946, transformId = 1945},
+	{position = Position(32874, 32263, 14), itemId = 3733, transformId = 3729},
+	{position = Position(32875, 32263, 14), itemId = 3734, transformId = 3730},
+	{position = Position(32874, 32264, 14), itemId = 3735, transformId = 3731},
+	{position = Position(32875, 32264, 14), itemId = 3736, transformId = 3732}
+}
+
+local sacrifices = {
+	{position = Position(32878, 32270, 14), itemId = 2016},
+	{position = Position(32881, 32267, 14), itemId = 2168},
+	{position = Position(32881, 32273, 14), itemId = 6300},
+	{position = Position(32884, 32270, 14), itemId = 1487}
+}
+
+function stoneTeleport.onAddItem(moveitem, tileitem, position)
+	local sacrificeItems, sacrificeItem = true
+	for i = 1, #sacrifices do
+		sacrificeItem = Tile(sacrifices[i].position):getItemById(sacrifices[i].itemId)
+		if not sacrificeItem then
+			sacrificeItems = false
+			break
+		end
+	end
+
+	if not sacrificeItems then
+		return true
+	end
+
+	local stonePosition = Position(32881, 32270, 14)
+	local stoneItem = Tile(stonePosition):getItemById(1355)
+	if stoneItem then
+		stoneItem:remove()
+	end
+
+	local teleportExists = Tile(stonePosition):getItemById(1387)
+	if not teleportExists then
+		local newItem = Game.createItem(1387, 1, stonePosition)
+		if newItem then
+			newItem:setActionId(9031)
+			stonePosition:sendMagicEffect(CONST_ME_MAGIC_BLUE)
+		end
+	end
+	return true
+end
+
+stoneTeleport:type("additem")
+stoneTeleport:aid(8034)
+stoneTeleport:register()
+
+local stoneTeleport = MoveEvent()
+
+function stoneTeleport.onStepIn(creature, item, position, fromPosition)
+	local player = creature:getPlayer()
+	if not player then
+		return false
+	end
+
+	local sacrificeItems, sacrificeItem = true
+	for i = 1, #sacrifices do
+		sacrificeItem = Tile(sacrifices[i].position):getItemById(sacrifices[i].itemId)
+		if not sacrificeItem then
+			sacrificeItems = false
+			break
+		end
+	end
+
+	if not sacrificeItems then
+		player:teleportTo(fromPosition)
+		fromPosition:sendMagicEffect(CONST_ME_TELEPORT)
+		return true
+	end
+
+	for i = 1, #sacrifices do
+		sacrificeItem = Tile(sacrifices[i].position):getItemById(sacrifices[i].itemId)
+		if sacrificeItem then
+			sacrificeItem:remove()
+		end
+	end
+
+	player:teleportTo(Position(32920, 32296, 13))
+	player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+	item:transform(1355)
+
+	local thing
+	for i = 1, #config do
+		thing = Tile(config[i].position):getItemById(config[i].itemId)
+		if thing then
+			thing:transform(config[i].transformId)
+		end
+	end
+	return true
+end
+
+stoneTeleport:type("stepin")
+stoneTeleport:aid(9031)
+stoneTeleport:register()
+
+local teleports = MoveEvent()
+
+local positions = {
+	[2250] = Position(32915, 32263, 14),
+	[2251] = Position(32946, 32270, 13),
+	[2252] = Position(32976, 32270, 14),
+	[2253] = Position(32933, 32282, 13),
+	[2254] = Position(32753, 32344, 14),
+	[2255] = Position(32753, 32344, 14)
+}
+
+function teleports.onStepIn(creature, item, position, fromPosition)
+	local player = creature:getPlayer()
+	if not player then
+		return false
+	end
+
+	player:teleportTo(positions[item.uid])
+	player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+	return true
+end
+
+teleports:type("stepin")
+teleports:uid(2250, 2251, 2252, 2253, 2254, 2255)
+teleports:register()
+
+local ticTeleport = MoveEvent()
+
+local config = {
+	{position = Position(32836, 32288, 14), itemId = 1387, transformId = 6299},
+	{position = Position(32836, 32278, 14), itemId = 1946, transformId = 1945},
+	{position = Position(32834, 32285, 14), itemId = 1946, transformId = 1945}
+}
+
+local tokens = {
+	{position = Position(32845, 32264, 14), itemId = 2639},
+	{position = Position(32843, 32266, 14), itemId = 2639},
+	{position = Position(32843, 32268, 14), itemId = 2639},
+	{position = Position(32845, 32268, 14), itemId = 2639},
+	{position = Position(32844, 32267, 14), itemId = 2639},
+	{position = Position(32840, 32269, 14), itemId = 2639},
+	{position = Position(32841, 32269, 14), itemId = 2638},
+	{position = Position(32840, 32268, 14), itemId = 2638},
+	{position = Position(32842, 32267, 14), itemId = 2638}
+}
+
+function ticTeleport.onStepIn(creature, item, position, fromPosition)
+	local player = creature:getPlayer()
+	if not player then
+		return true
+	end
+
+	local thing
+	for i = 1, #config do
+		thing = Tile(config[i].position):getItemById(config[i].itemId)
+		if thing then
+			thing:transform(config[i].transformId)
+		end
+	end
+
+	local token
+	for i = 1, #tokens do
+		token = Tile(tokens[i].position):getItemById(tokens[i].itemId)
+		if token then
+			token:remove()
+		end
+	end
+
+	player:teleportTo(Position(32874, 32275, 14))
+	player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+	return true
+end
+
+ticTeleport:type("stepin")
+ticTeleport:aid(9032)
+ticTeleport:register()
+
+local tower = MoveEvent()
+
+local config = {
+	[3048] = {storageValue = 1, toPosition = Position(32360, 31782, 7)}, -- Carlin
+	[3049] = {storageValue = 1, toPosition = Position(32369, 32241, 7)}, -- Thais
+	[3050] = {storageValue = 1, toPosition = Position(32750, 32344, 14)}, -- Dream Realm
+	[3051] = {storageValue = 2, toPosition = Position(32649, 31925, 11)}, -- Kazo
+	[3052] = {storageValue = 2, toPosition = Position(32732, 31634, 7)}, -- Ab
+	[3053] = {storageValue = 2, toPosition = Position(32181, 32436, 7)}, -- Fibula
+	[3054] = {storageValue = 4, toPosition = Position(33213, 32454, 1)}, -- Darashia
+	[3055] = {storageValue = 4, toPosition = Position(33194, 32853, 8)}, -- Ankrahmun
+	[3056] = {storageValue = 4, toPosition = Position(32417, 32139, 15)} -- Mintwalin
+}
+
+function tower.onStepIn(creature, item, position, fromPosition)
+	local player = creature:getPlayer()
+	if not player then
+		return true
+	end
+
+	local targetTeleport = config[item.uid]
+	if not targetTeleport then
+		return true
+	end
+
+	if (player:getStorageValue(PlayerStorageKeys.OutfitQuest.NightmareOutfit) >= targetTeleport.storageValue or player:getStorageValue(PlayerStorageKeys.OutfitQuest.BrotherhoodOutfit) >= targetTeleport.storageValue) and player:removeItem(5022, 1) then
+		player:teleportTo(targetTeleport.toPosition)
+	else
+		player:teleportTo(fromPosition)
+	end
+
+	player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+	return true
+end
+
+tower:type("stepin")
+tower:uid(3048, 3049, 3050, 3051, 3052, 3053, 3054, 3055, 3056)
+tower:register()
+
+local wallsTeleport = MoveEvent()
+
+local walls = {
+	Position(32760, 32288, 14),
+	Position(32761, 32288, 14),
+	Position(32762, 32288, 14),
+	Position(32763, 32288, 14),
+	Position(32764, 32288, 14),
+	Position(32764, 32289, 14),
+	Position(32764, 32290, 14),
+	Position(32764, 32291, 14),
+	Position(32764, 32292, 14),
+	Position(32763, 32292, 14),
+	Position(32762, 32292, 14),
+	Position(32761, 32292, 14),
+	Position(32760, 32292, 14),
+	Position(32760, 32291, 14),
+	Position(32760, 32290, 14),
+	Position(32760, 32289, 14)
+}
+
+function wallsTeleport.onStepIn(creature, item, position, fromPosition)
+	local player = creature:getPlayer()
+	if not player then
+		return true
+	end
+
+	for i = 1, #walls do
+		if Tile(walls[i]):hasFlag(TILESTATE_IMMOVABLEBLOCKSOLID) then
+			player:teleportTo(Position(32762, 32305, 14))
+			player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+			return true
+		end
+	end
+
+	player:teleportTo(Position(32852, 32287, 14))
+	player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+	return true
+end
+
+wallsTeleport:type("stepin")
+wallsTeleport:uid(9030)
+wallsTeleport:register()
+
+local deathRing = MoveEvent()
+
+local trees = {
+	Position(32857, 32231, 11),
+	Position(32857, 32232, 11),
+	Position(32857, 32233, 11)
+}
+
+function deathRing.onAddItem(moveitem, tileitem, position)
+	if moveitem.itemid ~= 6300 then
+		return true
+	end
+
+	moveitem:remove()
+	for i = 1, #trees do
+		local treeItem = Tile(trees[i]):getItemById(2722)
+		if treeItem then
+			treeItem:remove()
+			trees[i]:sendMagicEffect(CONST_ME_MAGIC_BLUE)
+		end
+	end
+	return true
+end
+
+deathRing:type("additem")
+deathRing:uid(2244)
+deathRing:register()
+
+local clockTile = MoveEvent()
+
+local words = {
+	'YOU ARE DREAMING !',
+	'WAKE UP !',
+	'TIC TAC',
+	'TAC',
+	'TIC'
+}
+
+function clockTile.onAddItem(moveitem, tileitem, position)
+	local player = creature:getPlayer()
+	if not player then
+		return true
+	end
+
+	position.x = position.x + math.random(-3, 3)
+	position.y = position.y + math.random(-3, 3)
+	player:say(words[math.random(#words)], TALKTYPE_MONSTER_SAY, false, 0, position)
+	return true
+end
+
+clockTile:type("additem")
+clockTile:aid(9049)
+clockTile:register()
+
+local deathRingTeleport = MoveEvent()
+
+local trees = {
+	Position(32857, 32231, 11),
+	Position(32857, 32232, 11),
+	Position(32857, 32233, 11)
+}
+
+function deathRingTeleport.onStepIn(creature, item, position, fromPosition)
+	local player = creature:getPlayer()
+	if not player then
+		return true
+	end
+
+	player:teleportTo(Position(32819, 32347, 9))
+	player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+	for i = 1, #trees do
+		if not Tile(trees[i]):getItemById(2722) then
+			Game.createItem(2722, 1, trees[i])
+			trees[i]:sendMagicEffect(CONST_ME_MAGIC_BLUE)
+		end
+	end
+	return true
+end
+
+deathRingTeleport:type("stepin")
+deathRingTeleport:uid(9234)
+deathRingTeleport:register()
+
+local riddleTeleport = MoveEvent()
+
+local destination = Position(32766, 32275, 14)
+local topLeftPosition = Position(32817, 32333, 9)
+local pillowPositions = {
+	{itemid = 1686, center = topLeftPosition + Position(2, 2, 0)},
+	{itemid = 1687, center = topLeftPosition + Position(2, 5, 0)},
+	{itemid = 1688, center = topLeftPosition + Position(5, 2, 0)},
+	{itemid = 1689, center = topLeftPosition + Position(5, 5, 0)}
+}
+
+function riddleTeleport.onStepIn(creature, item, position, fromPosition)
+	if not player:getPlayer() then
+		return true
+	end
+
+	local pillows = {}
+	for i = 1, #pillowPositions do
+		local pillowPos = pillowPositions[i]
+		for x = -1, 1 do
+			for y = -1, 1 do
+				local item = Tile(pillowPos.center + Position(x, y, 0)):getThing(1)
+				if not item or item.itemid ~= pillowPos.itemid then
+					player:teleportTo(fromPosition, true)
+					fromPosition:sendMagicEffect(CONST_ME_TELEPORT)
+					return true
+				end
+
+				pillows[#pillows + 1] = item
+			end
+		end
+	end
+
+	player:teleportTo(destination)
+	destination:sendMagicEffect(CONST_ME_TELEPORT)
+
+	for x = 1, 6 do
+		for y = 1, 6 do
+			local index = math.random(#pillows)
+			pillows[index]:moveTo(topLeftPosition + Position(x, y, 0))
+			table.remove(pillows, index)
+		end
+	end
+	return true
+end
+
+riddleTeleport:type("stepin")
+riddleTeleport:uid(50147)
+riddleTeleport:register()
--- data/scripts/quests/pits_of_Inferno.lua
+++ data/scripts/quests/pits_of_Inferno.lua
@@ -384,3 +384,455 @@ end
 
 mazeStone:aid(50160)
 mazeStone:register()
+
+local checkThrones = MoveEvent()
+
+local storages = {
+	[2090] = PlayerStorageKeys.PitsOfInferno.ThroneInfernatil,
+	[2091] = PlayerStorageKeys.PitsOfInferno.ThroneTafariel,
+	[2092] = PlayerStorageKeys.PitsOfInferno.ThroneVerminor,
+	[2093] = PlayerStorageKeys.PitsOfInferno.ThroneApocalypse,
+	[2094] = PlayerStorageKeys.PitsOfInferno.ThroneBazir,
+	[2095] = PlayerStorageKeys.PitsOfInferno.ThroneAshfalor,
+	[2096] = PlayerStorageKeys.PitsOfInferno.ThronePumin
+}
+
+function checkThrones.onStepIn(creature, item, position, fromPosition)
+	if not creature:isPlayer() then
+		return true
+	end
+
+	if creature:getStorageValue(storages[item.uid]) ~= 1 then
+		creature:teleportTo(fromPosition)
+		creature:say('You\'ve not absorbed energy from this throne.', TALKTYPE_MONSTER_SAY)
+	end
+	return true
+end
+
+checkThrones:type("stepin")
+checkThrones:uid(2090, 2091, 2092, 2093, 2094, 2095, 2096)
+checkThrones:register()
+
+local fireTiles = MoveEvent()
+
+local fires = {
+	[2040] = {vocationId = VOCATION_SORCERER, damage = 300},
+	[2041] = {vocationId = VOCATION_SORCERER, damage = 600},
+	[2042] = {vocationId = VOCATION_SORCERER, damage = 1200},
+	[2043] = {vocationId = VOCATION_SORCERER, damage = 2400},
+	[2044] = {vocationId = VOCATION_SORCERER, damage = 3600},
+	[2045] = {vocationId = VOCATION_SORCERER, damage = 7200},
+
+	[2046] = {vocationId = VOCATION_DRUID, damage = 300},
+	[2047] = {vocationId = VOCATION_DRUID, damage = 600},
+	[2048] = {vocationId = VOCATION_DRUID, damage = 1200},
+	[2049] = {vocationId = VOCATION_DRUID, damage = 2400},
+	[2050] = {vocationId = VOCATION_DRUID, damage = 3600},
+	[2051] = {vocationId = VOCATION_DRUID, damage = 7200},
+
+	[2052] = {vocationId = VOCATION_PALADIN, damage = 300},
+	[2053] = {vocationId = VOCATION_PALADIN, damage = 600},
+	[2054] = {vocationId = VOCATION_PALADIN, damage = 1200},
+	[2055] = {vocationId = VOCATION_PALADIN, damage = 2400},
+	[2056] = {vocationId = VOCATION_PALADIN, damage = 3600},
+	[2057] = {vocationId = VOCATION_PALADIN, damage = 7200},
+
+	[2058] = {vocationId = VOCATION_KNIGHT, damage = 300},
+	[2059] = {vocationId = VOCATION_KNIGHT, damage = 600},
+	[2060] = {vocationId = VOCATION_KNIGHT, damage = 1200},
+	[2061] = {vocationId = VOCATION_KNIGHT, damage = 2400},
+	[2062] = {vocationId = VOCATION_KNIGHT, damage = 3600},
+	[2063] = {vocationId = VOCATION_KNIGHT, damage = 7200}
+}
+
+function fireTiles.onStepIn(creature, item, position, fromPosition)
+	local player = creature:getPlayer()
+	if not player then
+		return true
+	end
+
+	local fire = fires[item.actionid]
+	if not fire then
+		return true
+	end
+
+	if player:getVocation():getBase():getId() == fire.vocationId then
+		doTargetCombat(0, player, COMBAT_FIREDAMAGE, -300, -300, CONST_ME_HITBYFIRE)
+	else
+		local combatType = COMBAT_FIREDAMAGE
+		if fire.damage > 300 then
+			combatType = COMBAT_PHYSICALDAMAGE
+		end
+		doTargetCombat(0, player, combatType, -fire.damage, -fire.damage, CONST_ME_FIREATTACK)
+	end
+	return true
+end
+
+fireTiles:type("stepin")
+
+for index, value in pairs(fires) do
+	fireTiles:aid(index)
+end
+
+fireTiles:register()
+
+local ladder = MoveEvent()
+
+function ladder.onStepIn(creature, item, position, fromPosition)
+	local player = creature:getPlayer()
+	if not player then
+		return true
+	end
+
+	local ladderPosition = Position(32854, 32321, 11)
+	local ladderItem = Tile(ladderPosition):getItemById(5543)
+	if not ladderItem then
+		Game.createItem(5543, 1, ladderPosition)
+		player:say('You hear a rumbling from far away.', TALKTYPE_MONSTER_SAY, false, player)
+	end
+	return true
+end
+
+ladder:type("stepin")
+ladder:aid(2002)
+ladder:register()
+
+local ladder = MoveEvent()
+
+function ladder.onStepOut(creature, item, position, fromPosition)
+	local player = creature:getPlayer()
+	if not player then
+		return true
+	end
+
+	local ladderPosition = Position(32854, 32321, 11)
+	local ladderItem = Tile(ladderPosition):getItemById(5543)
+	if ladderItem then
+		ladderItem:remove()
+		player:say('You hear a rumbling from far away.', TALKTYPE_MONSTER_SAY, false, player)
+	end
+	return true
+end
+
+ladder:type("stepout")
+ladder:aid(2002)
+ladder:register()
+
+local stone = MoveEvent()
+
+function stone.onStepIn(creature, item, position, fromPosition)
+	local player = creature:getPlayer()
+	if not player then
+		return true
+	end
+
+	local stonePosition = Position(32849, 32282, 10)
+	local stoneItem, leverItem = Tile(stonePosition):getItemById(1304), Tile(Position(32850, 32268, 10)):getItemById(1946)
+	if not stoneItem and leverItem then
+		Game.createItem(1304, 1, stonePosition)
+		leverItem:transform(1945)
+		player:say('You hear a rumbling from far away.', TALKTYPE_MONSTER_SAY, false, player)
+	end
+	return true
+end
+
+stone:type("stepin")
+stone:aid(4001)
+stone:register()
+
+local drawbridge = MoveEvent()
+
+local bridgePosition = Position(32851, 32309, 11)
+local relocatePosition = Position(32852, 32310, 11)
+local dirtIds = {4808, 4810}
+
+function drawbridge.onStepIn(creature, item, position, fromPosition)
+	if not creature:isPlayer() then
+		return true
+	end
+
+	local tile = Tile(bridgePosition)
+	local lavaItem = tile:getItemById(598)
+	if lavaItem then
+		lavaItem:transform(1284)
+
+		local dirtItem
+		for i = 1, #dirtIds do
+			dirtItem = tile:getItemById(dirtIds[i])
+			if dirtItem then
+				dirtItem:remove()
+			end
+		end
+	end
+	return true
+end
+
+drawbridge:type("stepin")
+drawbridge:aid(4002)
+drawbridge:register()
+
+local drawbridge = MoveEvent()
+
+function drawbridge.onStepOut(creature, item, position, fromPosition)
+	if not creature:isPlayer() then
+		return true
+	end
+
+	local tile = Tile(bridgePosition)
+	local bridgeItem = tile:getItemById(1284)
+	if bridgeItem then
+		tile:relocateTo(relocatePosition)
+		bridgeItem:transform(598)
+
+		for i = 1, #dirtIds do
+			Game.createItem(dirtIds[i], 1, bridgePosition)
+		end
+	end
+	return true
+end
+
+drawbridge:type("stepout")
+drawbridge:aid(4002)
+drawbridge:register()
+
+local shortcuts = MoveEvent()
+
+local storages = {
+	[8816] = PlayerStorageKeys.PitsOfInferno.ShortcutHub,
+	[8817] = PlayerStorageKeys.PitsOfInferno.ShortcutLevers
+}
+
+function shortcuts.onStepIn(creature, item, position, fromPosition)
+	local player = creature:getPlayer()
+	if not player then
+		return true
+	end
+
+	local cutoffStorage = storages[item.actionid]
+	if player:getStorageValue(cutoffStorage) ~= 1 then
+		player:setStorageValue(cutoffStorage, 1)
+	end
+	return true
+end
+
+shortcuts:type("stepin")
+shortcuts:aid(8816, 8817)
+shortcuts:register()
+
+local thrones = MoveEvent()
+
+local config = {
+	[2080] = {storage = PlayerStorageKeys.PitsOfInferno.ThroneInfernatil, text = 'You have touched Infernatil\'s throne and absorbed some of his spirit.', effect = CONST_ME_FIREAREA, toPosition = Position(32909, 32211, 15)},
+	[2081] = {storage = PlayerStorageKeys.PitsOfInferno.ThroneTafariel, text = 'You have touched Tafariel\'s throne and absorbed some of his spirit.', effect = CONST_ME_MORTAREA, toPosition = Position(32761, 32243, 15)},
+	[2082] = {storage = PlayerStorageKeys.PitsOfInferno.ThroneVerminor, text = 'You have touched Verminor\'s throne and absorbed some of his spirit.', effect = CONST_ME_POISONAREA, toPosition = Position(32840, 32327, 15)},
+	[2083] = {storage = PlayerStorageKeys.PitsOfInferno.ThroneApocalypse, text = 'You have touched Apocalypse\'s throne and absorbed some of his spirit.', effect = CONST_ME_EXPLOSIONAREA, toPosition = Position(32875, 32267, 15)},
+	[2084] = {storage = PlayerStorageKeys.PitsOfInferno.ThroneBazir, text = 'You have touched Bazir\'s throne and absorbed some of his spirit.', effect = CONST_ME_MAGIC_GREEN, toPosition = Position(32745, 32385, 15)},
+	[2085] = {storage = PlayerStorageKeys.PitsOfInferno.ThroneAshfalor, text = 'You have touched Ashfalor\'s throne and absorbed some of his spirit.', effect = CONST_ME_FIREAREA, toPosition = Position(32839, 32310, 15)},
+	[2086] = {storage = PlayerStorageKeys.PitsOfInferno.ThronePumin, text = 'You have touched Pumin\'s throne and absorbed some of his spirit.', effect = CONST_ME_MORTAREA, toPosition = Position(32785, 32279, 15)}
+}
+
+function thrones.onStepIn(creature, item, position, fromPosition)
+	if not creature:isPlayer() then
+		return true
+	end
+
+	local throne = config[item.uid]
+	if not throne then
+		return true
+	end
+
+	local cStorage = throne.storage
+	if creature:getStorageValue(cStorage) ~= 1 then
+		creature:setStorageValue(cStorage, 1)
+		creature:getPosition():sendMagicEffect(throne.effect)
+		creature:say(throne.text, TALKTYPE_MONSTER_SAY)
+	else
+		creature:teleportTo(throne.toPosition)
+		creature:getPosition():sendMagicEffect(CONST_ME_MORTAREA)
+		creature:say('Begone!', TALKTYPE_MONSTER_SAY)
+	end
+	return true
+end
+
+thrones:type("stepin")
+thrones:uid(2080, 2081, 2082, 2083, 2084, 2085, 2086)
+thrones:register()
+
+local tibleTile = MoveEvent()
+
+local destinations = {
+	[2000] = Position(32791, 32331, 10),
+	[2001] = Position(32791, 32327, 10)
+}
+
+function tibleTile.onStepIn(creature, item, position, fromPosition)
+	local player = creature:getPlayer()
+	if not player then
+		return false
+	end
+
+	if player:getItemCount(1970) < 1 then
+		player:teleportTo(fromPosition)
+		return true
+	end
+
+	player:teleportTo(destinations[item.uid])
+	player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+	return true
+end
+
+tibleTile:type("stepin")
+tibleTile:aid(2000, 2001)
+tibleTile:register()
+
+local tileTeleports = MoveEvent()
+
+local config = {
+	[28810] = Position(32838, 32304, 9),
+	[28811] = Position(32839, 32320, 9),
+	[28812] = Position(32844, 32310, 9),
+	[28813] = Position(32847, 32307, 9),
+	[28814] = Position(32856, 32306, 9),
+	[28815] = Position(32827, 32308, 9),
+	[28816] = Position(32840, 32317, 9),
+	[28817] = Position(32855, 32296, 9),
+	[28818] = Position(32857, 32307, 9),
+	[28819] = Position(32856, 32289, 9),
+	[28820] = Position(32843, 32313, 9),
+	[28821] = Position(32861, 32320, 9),
+	[28822] = Position(32841, 32323, 9),
+	[28823] = Position(32847, 32287, 9),
+	[28824] = Position(32854, 32323, 9),
+	[28825] = Position(32855, 32304, 9),
+	[28826] = Position(32841, 32323, 9),
+	[28827] = Position(32861, 32317, 9),
+	[28828] = Position(32827, 32314, 9),
+	[28829] = Position(32858, 32296, 9),
+	[28830] = Position(32861, 32301, 9),
+	[28831] = Position(32855, 32321, 9),
+	[28832] = Position(32855, 32320, 9),
+	[28833] = Position(32855, 32318, 9),
+	[28834] = Position(32855, 32319, 9)
+}
+
+function tileTeleports.onStepIn(creature, item, position, fromPosition)
+	local player = creature:getPlayer()
+	if not player then
+		return true
+	end
+
+	local targetPosition = config[item.actionid]
+	if not targetPosition then
+		return true
+	end
+
+	player:teleportTo(targetPosition)
+	return true
+end
+
+tileTeleports:type("stepin")
+
+for index, value in pairs(config) do
+	tileTeleports:aid(index)
+end
+
+tileTeleports:register()
+
+local trap = MoveEvent()
+
+function trap.onStepIn(creature, item, position, fromPosition)
+	local player = creature:getPlayer()
+	if not player then
+		return true
+	end
+
+	position.z = position.z + 1
+	player:teleportTo(position)
+	position:sendMagicEffect(CONST_ME_FIREATTACK)
+	return true
+end
+
+trap:type("stepin")
+trap:aid(7799)
+trap:register()
+
+local secondTrap = MoveEvent()
+
+local stonePosition = Position(32826, 32274, 11)
+
+function removeStone()
+	local stoneItem = Tile(stonePosition):getItemById(1285)
+	if stoneItem then
+		stoneItem:remove()
+		stonePosition:sendMagicEffect(CONST_ME_POFF)
+	end
+end
+
+function secondTrap.onStepIn(creature, item, position, fromPosition)
+	local player = creature:getPlayer()
+	if not player then
+		return true
+	end
+
+	player:teleportTo(Position(32826, 32273, 12))
+	player:getPosition():sendMagicEffect(CONST_ME_EXPLOSIONAREA)
+	Game.createItem(1285, 1, stonePosition)
+	addEvent(removeStone, 10 * 1000)
+	return true
+end
+
+secondTrap:type("stepin")
+secondTrap:uid(3303)
+secondTrap:register()
+
+local bazirTiles = MoveEvent()
+
+local config = {
+	[16772] = Position(32754, 32365, 15),
+	[16773] = Position(32725, 32381, 15),
+	[16774] = Position(32827, 32241, 12),
+	[50082] = Position(32745, 32394, 14),
+	[50083] = Position(32745, 32394, 14)
+}
+
+function bazirTiles.onStepIn(creature, item, position, fromPosition)
+	local player = creature:getPlayer()
+	if not player then
+		return true
+	end
+
+	local targetPosition = config[item.actionid]
+	if not targetPosition then
+		return true
+	end
+
+	player:teleportTo(targetPosition)
+	return true
+end
+
+bazirTiles:type("stepin")
+bazirTiles:aid(16772, 16773, 16774, 50082, 50083)
+bazirTiles:register()
+
+local puminTeleport = MoveEvent()
+
+function puminTeleport.onStepIn(creature, item, position, fromPosition)
+	local player = creature:getPlayer()
+	if not player then
+		return true
+	end
+
+	if player:getStorageValue(PlayerStorageKeys.PitsOfInferno.Pumin) > 8 then
+		player:teleportTo(Position(32786, 32308, 15))
+		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+	else
+		player:teleportTo(fromPosition)
+		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, 'To enter Pumin\'s domain you must gain permission from the bureaucrats.')
+	end
+	return true
+end
+
+puminTeleport:type("stepin")
+puminTeleport:aid(50087)
+puminTeleport:register()
